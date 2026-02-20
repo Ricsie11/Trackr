f06c20484c9360eabb0e2ba3500ae16f4d09a29d
Alfred Wisdom <wisdomalfred964@gmail.com>
2026-02-20 17:12:05 +0100

    feat: updated footer link

--- Trackr-Frontend/src/components/layout/Footer.jsx
+++ Trackr-Frontend/src/components/layout/Footer.jsx
@@ -65,7 +65,8 @@ const Footer = () => {
             <Github className="w-5 h-5" />
           </a>
           <a
-            href="#"
+            href="https://github.com/Ricsie11/"
+            target="_blank"
             className="p-2 rounded-lg bg-white/5 border border-white/10 text-slate-400 hover:text-white hover:bg-white/10 hover:border-white/20 transition-all"
           >
             <Linkedin className="w-5 h-5" />
